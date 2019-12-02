231d500597e76aba907a2493c87aba7164404526
kreatinbiceps <drogoxx@protonamil.com>
2019-12-02 15:58:31 +0100

    modified:   tic.py

--- tic.py
+++ tic.py
@@ -1,4 +1,4 @@
-
+import sys
 
 rows = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
 
@@ -28,10 +28,78 @@ def drawTablep2(player2_choice):
 
 
 def count():
-	if rows[0] and rows[1] and rows[2] == "X":
-		print ("you have 3 in a row - P1 Wins")
-	if rows[0] and rows[1] and rows[2] == "O":
-		print ("you have 3 in a row - P2 Wins")
+	win = "you have 3 in a row - P1 Wins"
+	print(rows)
+#	Horizontal X
+
+	if rows[0] == "X" and rows[1] == "X" and rows[2] == "X":
+		print(win)
+		sys.exit()
+	elif rows[3] == "X" and rows[4] == "X" and rows[5] == "X":
+		print(win)
+		sys.exit()
+
+	elif rows[6] == "X"  and rows[7] == "X" and rows[8] == "X":
+		print(win)
+		sys.exit()
+
+#	Vertical X
+	elif rows[0] == "X" and rows[3] == "X" and rows[6] == "X":
+		print(win)
+		sys.exit()
+	elif rows[1] == "X" and rows[4] == "X" and rows[7] == "X":
+		print(win)
+		sys.exit()
+	elif rows[2] == "X" and rows[5] == "X" and rows[8] == "X":
+		print(win)
+		sys.exit()
+
+#	Diagonal X
+	elif rows[0] == "X" and rows[4] == "X" and rows[8] == "X":
+		print(win)
+		sys.exit()
+
+	elif rows[6] == "X" and rows[4] == "X" and rows[2] == "X":
+		print(win)
+		sys.exit()
+
+#       Horizontal O
+	elif rows[0] == "O" and rows[1] == "O" and rows[2] == "O":
+		print(win)
+		sys.exit()
+
+	elif rows[3] == "O" and rows[4] == "O" and rows[5] == "O":
+		print(win)
+		sys.exit()
+
+	elif rows[6] == "O" and rows[7] == "O" and rows[8] == "O":
+		print(win)
+		sys.exit()
+
+#       Vertical O
+	elif rows[0] == "O" and rows[3] == "O" and rows[6] == "O":
+		print(win)
+		sys.exit()
+
+	elif rows[1] == "O" and rows[4] == "O" and rows[7] == "O":
+		print(win)
+		sys.exit()
+	elif rows[2] == "O" and rows[5] == "O" and rows[8] == "O":
+		print(win)
+		sys.exit()
+
+#       Diagonal O
+	elif rows[0] == "O" and rows[4] == "O" and rows[8] == "O":
+		print(win)
+		sys.exit()
+
+	elif rows[6] == "O" and rows[4] == "O" and rows[2] == "O":
+		print(win)
+		sys.exit()
+
+
+
+
 
 
 
